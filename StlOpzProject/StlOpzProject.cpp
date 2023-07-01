291050ab5a5f4398b743277f9d4a8641c55ad410
maxefimov <efimov.top.academy@gmail.com>
2023-07-01 13:22:54 +0300

    01-07-23

--- StlOpzProject/StlOpzProject.cpp
+++ StlOpzProject/StlOpzProject.cpp
@@ -3,12 +3,13 @@
 
 int main()
 {
-    MathExpression expr("5 + 6 * 7");
-    expr["d"] = 23.5;
+    MathExpression expr("(5 + 6) * 7 + sin(1 + 1)");
+    expr["x"] = 2;
     //int p = expr.CheckBrackets();
 
     expr.OpzCreate();
     cout << expr.ExpressionOpz() << "\n";
+    cout << expr.Expression() << " = " << expr.OpzCalculate() << "\n";
     
     
 }
